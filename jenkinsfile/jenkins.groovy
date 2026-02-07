1de713f98152520199fedb475b78775fdcff72aa
DMLakmali <lakmalidissanayaka203@gmail.com>
2026-02-07 11:31:29 +0530

    add jenkins file

---
+++ jenkinsfile/jenkins.groovy
@@ -0,0 +1,39 @@
+
+pipeline {
+    agent any
+
+    tools {
+        jdk 'jdk21'
+        maven 'maven3'
+    }
+
+    stages {
+
+        stage('Checkout') {
+            steps {
+                checkout scm
+            }
+        }
+
+        stage('Build & Test') {
+            steps {
+                sh 'mvn clean test'
+            }
+        }
+
+        stage('Cucumber Report') {
+            steps {
+                cucumber buildStatus: 'UNSTABLE',
+                        reportTitle: 'Automation Test Report',
+                        fileIncludePattern: '**/cucumber*.json'
+            }
+        }
+    }
+
+    post {
+        always {
+            archiveArtifacts artifacts: '**/target/*.jar', fingerprint: true
+            junit '**/target/surefire-reports/*.xml'
+        }
+    }
+}
